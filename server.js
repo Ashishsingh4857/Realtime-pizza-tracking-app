53f094a27774f05e689a262fe948895ce9e4fd00
ashish singh <ashishsingh4857@gmail.com>
2024-02-24 14:03:16 +0530

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+// set template engine
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+app.use(expressLayout);
+
+app.listen(PORT, () => {
+  console.log(`listing to port ${PORT}`);
+});
